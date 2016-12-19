a68e2902ef41545c660b4441cf9eea8f0334ab8b
waldhol <holger.waldmann@web.de>
2016-12-19 21:44:09 +0100

    Usage example

--- aiocall/__init__.py
+++ aiocall/__init__.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 import asyncio
-import logging
 
 def call_soon_periodic(interval, callback, *args, **kwargs):
     """Arrange for the `callback` to be called as soon as possible and
@@ -41,21 +40,21 @@ def call_later_periodic(delay, interval, callback, *args, **kwargs):
     return asyncio.Task(coro(), **kwargs)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
-    logger = logging.getLogger(__name__)
     loop = asyncio.get_event_loop()
 
     def do_something(what):
-        logger.info('doing %s...', what)
+        print('doing %s...' % what)
 
-    logger.info('setup timers')
     timer1 = call_soon_periodic(1.0, do_something, 'stuff')
     timer2 = call_later_periodic(0.5, 1.0, do_something, 'other stuff')
+
     loop.call_later(2.7, timer1.cancel)
     loop.call_later(2.8, timer2.cancel)
+
     loop.call_later(2.9, loop.stop)
-    logger.info('starting loop')
+
+    print('starting loop')
     loop.run_forever()
-    logger.info('loop done')
+    print('loop done')
 
     loop.close()

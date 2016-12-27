65fea765dda96cd8fe12d712c484f01f33814f14
waldhol <holger.waldmann@web.de>
2016-12-27 21:26:21 +0100

    first step for #1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aiocall',
+      version='0.1',
+      description='The missing call methods for python asyncio',
+      url='http://github.com/waldhol/aiocall',
+      author='Holger Waldmann',
+      author_email='holger.waldmann@web.de',
+      license='Public Domain',
+      packages=['aiohttp'],
+      zip_safe=False)

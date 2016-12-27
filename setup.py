b3d8edc43e5e151c0d33ad4fbc7eb89c62ca69ce
waldhol <holger.waldmann@web.de>
2016-12-27 21:53:07 +0100

    some more boilerplate for #1

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
+import os
+
 from setuptools import setup
 
 setup(name='aiocall',
       version='0.1',
       description='The missing call methods for python asyncio',
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read().strip(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet',
+      ],
       url='http://github.com/waldhol/aiocall',
       author='Holger Waldmann',
       author_email='holger.waldmann@web.de',
